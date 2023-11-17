f68a741336cd0feb58792401299c4a857c62e68b
William Lucas <106882851+WilliamLucasSilva@users.noreply.github.com>
2023-11-17 17:44:25 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -16,7 +16,7 @@ class produto{
 
 let dados = [
     new produto('iPhone 14', 'celular', '', 'iphone_14.jpg', '4.499,99'),
-    new produto('iPhone 13 Pro Max', 'celular', '', 'iphone_13.jpg', '3.999,99'),
+    new produto('iPhone 13', 'celular', '', 'iphone_13.jpg', '3.999,99'),
     new produto('iPhone 12', 'celular', '', 'iphone_12.jpg', '3.499,99')
 ]
 
